25e2d575b3715815a47f7d92e7cc5ab383f4782d
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-27 18:42:00 -0500

    Setting up react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Posts from "./pages/Posts/Posts";
+import PostAdmin from "./pages/PostAdmin/PostAdmin";
+import PostPreview from "./pages/PostPreview/PostPreview";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Posts />} />
+          <Route path="posts">
+            <Route path="create" element={<PostAdmin />} />
+            <Route path=":postId">
+              <Route index element={<PostAdmin />} />
+              <Route path="preview" element={<PostPreview />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
