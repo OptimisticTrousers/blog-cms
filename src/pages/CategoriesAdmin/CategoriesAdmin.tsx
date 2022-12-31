ce4b47a31c43a44500a67a30ac6a83a29dfa0562
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-31 08:59:56 -0500

    CategoriesAdmin creation of categories works

---
+++ src/pages/CategoriesAdmin/CategoriesAdmin.tsx
@@ -0,0 +1,72 @@
+import { Box, MenuButton, Button, Heading } from "@chakra-ui/react";
+import { TextField } from "@mui/joy";
+import axios from "axios";
+import React, { FC, useRef } from "react";
+import { Form } from "react-bootstrap";
+import { Category, UserCategory } from "../../atoms";
+import { apiDomain } from "../../utils";
+
+interface Props {
+  category?: Category;
+  deleteButton?: HTMLButtonElement;
+  handleEditCategory?: any;
+}
+
+const CategoriesAdmin: FC<Props> = ({
+  category,
+  deleteButton,
+  handleEditCategory,
+}) => {
+  const nameRef = useRef<HTMLDivElement | null>(null);
+
+  const handleCreateCategory = async (values: UserCategory) => {
+    try {
+      const { data } = await axios.post(`${apiDomain()}/categories`, values);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    if (!nameRef.current) return;
+
+    event.preventDefault();
+    const name = nameRef.current.children[0].children[0].value;
+
+    const category = { name };
+
+    if (handleEditCategory) {
+      handleEditCategory(category);
+    } else {
+      handleCreateCategory(category);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Heading size={"xl"} textAlign={"center"}>
+        Create a New Category!
+      </Heading>
+      <TextField
+        size="lg"
+        placeholder="Category Title"
+        sx={{ width: "100%", marginTop: "1rem" }}
+        ref={nameRef}
+      />
+      <Button
+        color="white"
+        bgColor="#3f87e5"
+        borderRadius={8}
+        width={"100%"}
+        marginTop={"1rem"}
+        type="submit"
+      >
+        Save
+      </Button>
+      <Box>{deleteButton}</Box>
+    </Form>
+  );
+};
+
+export default CategoriesAdmin;
