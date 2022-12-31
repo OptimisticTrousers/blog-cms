565c3fb9fe3772c57d4be9c38588f50e0e0f143e
OptimisticTrousers <tony.isern1@gmail.com>
2022-12-30 23:25:28 -0500

    Changing navigation bar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,3 +1,4 @@
+import { NavDropdown } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -7,7 +8,7 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 
 function NavigationBar() {
   return (
-    <Navbar bg="light" expand="sm" className="mb-3" >
+    <Navbar bg="light" expand="sm" className="mb-3">
       <Container fluid>
         <Navbar.Brand href="/">Blog CMS</Navbar.Brand>
         <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
@@ -18,13 +19,40 @@ function NavigationBar() {
         >
           <Offcanvas.Header closeButton>
             <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`}>
-              Offcanvas
+              Navigation
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3">
               <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/posts/create">Create Post</Nav.Link>
+              <NavDropdown
+                title="Lists"
+                id={`offcanvasNavbarDropdown-expand-sm`}
+              >
+                <NavDropdown.Item href="/posts">
+                  All Posts
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/categories">
+                  All Categories
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/tags">
+                  All Tags
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown
+                title="Create"
+                id={`offcanvasNavbarDropdown-expand-sm`}
+              >
+                <NavDropdown.Item href="/posts/create">
+                  Create Post
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/categories/create">
+                  Create Category
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/tags/create">
+                  Create Tag
+                </NavDropdown.Item>
+              </NavDropdown>
             </Nav>
             <Form className="d-flex">
               <Form.Control
