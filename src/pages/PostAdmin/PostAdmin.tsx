df6ec0f0d78774a18eddfe28e7a346b77b48adb6
OptimisticTrousers <tony.isern1@gmail.com>
2023-01-01 00:15:54 -0500

    Finalizing changes

--- src/pages/PostAdmin/PostAdmin.tsx
+++ src/pages/PostAdmin/PostAdmin.tsx
@@ -252,7 +252,7 @@ const PostAdmin: FC<Props> = ({
                 height: 500,
                 menubar: false,
                 plugins:
-                  "print preview importcss tinydrive searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons",
+                  "preview importcss tinydrive searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons",
 
                 toolbar:
                   "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment",
